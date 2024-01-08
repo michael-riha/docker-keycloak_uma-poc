1f5b0c4c584ba3d4826e85edf38a033680f748ee
michael.riha <michael.riha@bitmovin.com>
2024-01-08 13:46:11 +0100

    updated
    - requirements.txt
    - added login flow for fastAPI  ⚠️ NOT WORKING, YET!
    - added OIDC browser redirectURL to .env vars

--- python/fastAPI/app/main.py
+++ python/fastAPI/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 import os
 
 
@@ -17,8 +17,78 @@ def read_root():
     print("something")
     return {"Hello": "World"}
 
-from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection, KeycloakUMA
 
+# ----------- AUTH ----------- 
+issuer = os.environ.get("KEYCLOAK_URL")+"realms/"+os.environ.get("KEYCLOAK_REALM")
+clientId = os.environ.get("KEYCLOAK_CLIENT_ID")
+clientSecret = os.environ.get("KEYCLOAK_CLIENT_SECRET")
+oidcDiscoveryUrl = f'{issuer}/.well-known/openid-configuration'
+
+#scope = 'openid profile email'
+scope = 'email'
+# from authlib.integrations.requests_client import OAuth2Session
+from authlib.integrations.requests_client import OAuth2Session
+import requests
+import secrets
+from urllib.parse import urlencode, quote
+@app.get("/private")
+def get_private(request: Request):
+    try:
+        # Accessing the Authorization header
+        authorization_header = request.headers.get("Authorization")
+
+        if authorization_header is None:
+            raise HTTPException(status_code=401, detail="Authorization header is missing")
+        
+        # Check if the Authorization header starts with 'Bearer'
+        if not authorization_header.startswith("Bearer "):
+            raise HTTPException(status_code=401, detail="Invalid authorization header")
+        # Extract the token after 'Bearer '
+        bearer_token = authorization_header.split(" ")[1]
+
+    except HTTPException as e:
+        # Catch HTTPException and perform a redirect
+        if e.status_code == 401:
+            resp = requests.get(url=oidcDiscoveryUrl)
+            auth_url = resp.json()["authorization_endpoint"]
+            #HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
+            auth_url = auth_url.replace(os.environ.get("KEYCLOAK_URL"), os.environ.get("KEYCLOAK_BROWSER_URL"))
+            response_type = 'code'
+            client_id = 'myclient'
+            redirect_uri = request.url.scheme+"://"+request.url.netloc+"/callback"
+            # Encoding the redirect_uri
+            redirect_uri= quote(redirect_uri, safe='')
+            state = 'xyz123'
+            # Generate a random string for nonce
+            nonce_value = secrets.token_urlsafe(16)  # Adjust the length as needed (16 bytes here)
+            login_url= f"{auth_url}?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}&nonce={nonce_value}"
+
+            return RedirectResponse(login_url)
+        else:
+                # Handle other HTTPException cases here
+                raise e  # Re-raise the exception if not handled
+
+    return {"User-Agent": user_agent, "Content-Type": content_type}
+
+
+@app.get("/callback")
+def get_callback(request: Request):
+    print("something")
+    resp= requests.get(url=oidcDiscoveryUrl)
+    token_url= resp.json()["token_endpoint"]
+    # scope = 'openid email profile'
+    # client = OAuth2Session(clientId, clientSecret, scope=scope)
+    redirect_uri = request.url.scheme+"://"+request.url.netloc+"/callback"
+    # Encoding the redirect_uri
+    redirect_uri= quote(redirect_uri, safe='')
+    # https://github.com/lepture/authlib/blob/c8f154ff35459f79cc04f4c214601d25d716ddd4/authlib/oauth2/client.py#L160
+    client = OAuth2Session(clientId, clientSecret, scope=scope, redirect_uri=redirect_uri)
+    token_resp = client.fetch_token(url=token_url, authorization_response=request.url._url, authorization_code='authorization_code')
+    # https://github.com/lepture/authlib/blob/c8f154ff35459f79cc04f4c214601d25d716ddd4/docs/specs/rfc7636.rst#L83
+    return {"this is": "the callback"}
+
+# ----------- UMA ----------- 
+from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection, KeycloakUMA
 @app.get("/uma")
 def get_uma():
     # do we have all env vars ready?
