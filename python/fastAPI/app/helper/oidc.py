0ab0e6eacfb9af7db73cbda0feae78cc7a1003cb
Maxim Semenov <125667953+oranginaround@users.noreply.github.com>
2024-02-21 09:14:54 +0100

    UMA usage example (#1)
    UMA usage example + refactoring
    
    ---------
    
    Co-authored-by: Balazs Stasz <balazs.stasz@bonrepublic.com>

--- python/fastAPI/app/helper/oidc.py
+++ python/fastAPI/app/helper/oidc.py
@@ -1,21 +1,20 @@
 import os
-
-issuer = os.environ.get("KEYCLOAK_URL")+"realms/"+os.environ.get("KEYCLOAK_REALM")
-clientId = os.environ.get("KEYCLOAK_CLIENT_ID")
-clientSecret = os.environ.get("KEYCLOAK_CLIENT_SECRET")
-oidcDiscoveryUrl = f'{issuer}/.well-known/openid-configuration'
-
-scope = 'openid email profile'
+import secrets
+from urllib.parse import quote
 
 import requests
-import secrets
-from urllib.parse import urlencode, quote
-
-from fastapi import FastAPI, Request, HTTPException
+from authlib.jose import jwt
+from authlib.jose.errors import *
+from fastapi import Request, HTTPException
 from fastapi.responses import RedirectResponse
-def check_bearer (request: Request):
+
+from oauth_client import OAuthClient
+
+
+def check_bearer(request: Request):
     try:
         # Accessing the Authorization header
+
         authorization_header = request.headers.get("Authorization")
         if authorization_header is None:
             raise HTTPException(status_code=401, detail="Authorization header is missing")
@@ -28,54 +27,63 @@ def check_bearer (request: Request):
     except HTTPException as e:
         # Catch HTTPException and perform a redirect
         if e.status_code == 401:
-            resp = requests.get(url=oidcDiscoveryUrl)
+            client = OAuthClient()
+            resp = requests.get(url=client.oidcDiscoveryUrl)
             auth_url = resp.json()["authorization_endpoint"]
-            #HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
+            # HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
             auth_url = auth_url.replace(os.environ.get("KEYCLOAK_URL"), os.environ.get("KEYCLOAK_BROWSER_URL"))
             response_type = 'code'
             client_id = 'myclient'
-            redirect_uri = request.url.scheme+"://"+request.url.netloc+"/callback"
+            redirect_uri = request.url.scheme + "://" + request.url.netloc + "/callback"
             # Encoding the redirect_uri
             # redirect_uri= quote(redirect_uri, safe='')
             state = 'xyz123'
             # Generate a random string for nonce
             nonce_value = secrets.token_urlsafe(16)  # Adjust the length as needed (16 bytes here)
-            login_url= f"{auth_url}?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}&nonce={nonce_value}"
+
+            login_url = f"{auth_url}?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={client.scope}&state={state}&nonce={nonce_value}"
             return RedirectResponse(login_url)
         else:
-                # Handle other HTTPException cases here
-                raise e  # Re-raise the exception if not handled
+            # Handle other HTTPException cases here
+            raise e  # Re-raise the exception if not handled
+
 
 def get_discovery_response():
-     return requests.get(url=oidcDiscoveryUrl)
+    client = OAuthClient()
+    return requests.get(url=client.oidcDiscoveryUrl)
+
 
 def get_discovery_response_dict():
-     return get_discovery_response().json()
+    return get_discovery_response().json()
+
 
 def encode_url(url):
     return quote(url, safe='')
 
-def redirect_uri_relative(request:Request, path:str):
-    return request.url.scheme+"://"+request.url.netloc+path
 
-def clean_browser_url(url:str):
-    #HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
+def redirect_uri_relative(request: Request, path: str):
+    return request.url.scheme + "://" + request.url.netloc + path
+
+
+def clean_browser_url(url: str):
+    # HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
     return url.replace(os.environ.get("KEYCLOAK_URL"), os.environ.get("KEYCLOAK_BROWSER_URL"))
 
-from authlib.jose import jwt
-from authlib.jose.errors import *
+
 def get_public_keys():
     # Fetching OIDC discovery information
-    discovery_info= get_discovery_response_dict()
-     # Fetching JWKS URI from discovery information
+    discovery_info = get_discovery_response_dict()
+    # Fetching JWKS URI from discovery information
     jwks_uri = discovery_info['jwks_uri']
     # Fetching JWKS (JSON Web Key Set) from JWKS URI
     jwks_response = requests.get(jwks_uri)
     jwks = jwks_response.json()
     # Fetching the JWT certificate from JWKS https://docs.authlib.org/en/latest/jose/jwt.html#use-dynamic-keys
     return jwks
+
+
 # https://www.scottbrady91.com/python/authlib-python-jwt
-def verify_jwt(token:str):
+def verify_jwt(token: str):
     # Fetching the JWT certificate from JWKS
     jwks = get_public_keys()
     # Verify the JWT token using the retrieved certificates
@@ -105,6 +113,6 @@ def verify_jwt(token:str):
     except InvalidClaimError as e:
         # Invalid claims
         raise e
-    except InvalidTokenError:
+    except InvalidTokenError as e:
         # Invalid token
         raise e
--- python/fastAPI/app/main.py
+++ python/fastAPI/app/main.py
@@ -1,14 +1,43 @@
-from fastapi import FastAPI, Request, HTTPException
 import os
+from typing import Annotated
+
+import requests
+import uvicorn
+from authlib.jose.errors import *
+from fastapi import Depends, status
+from fastapi import FastAPI, Request, HTTPException
+# https://fastapi.tiangolo.com/advanced/custom-response/#html-response
+from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+# https://fastapi.tiangolo.com/es/advanced/custom-response/#redirectresponse
+from fastapi.responses import RedirectResponse
+from fastapi.security import OAuth2PasswordBearer
+from keycloak.exceptions import KeycloakPostError
+
+# HACK
+# TODO put the helpers into a dedicated folder !WARNING! this needs to be mounted in python as well!!
+# import sys
+# # Get the absolute path of the parent folder
+# parent_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# # Add the parent folder to the Python path
+# sys.path.append(parent_folder_path)
+from helper.oidc import check_bearer
+from helper.oidc import verify_jwt
+from keycloack_client import KeycloakClient
+from oauth_client import OAuthClient
+from schemas import AuthData
 
 app = FastAPI()
 
-# https://fastapi.tiangolo.com/es/advanced/custom-response/#redirectresponse
-from fastapi.responses import RedirectResponse
+# TODO: just for feasibility -> SHARED STATE https://www.vidavolta.io/sharing-memory/
+state = {}  # HACK store the bearer_token for now!
+
+
 @app.get("/")
 def redirect_to_home(request: Request):
-    #return {"raw_url": request.url._url}
-    return RedirectResponse(request.url._url+"home")
+    # return {"raw_url": request.url._url}
+    return RedirectResponse(request.url._url + "home")
+
 
 @app.get("/home")
 def read_root():
@@ -16,89 +45,45 @@ def read_root():
     return {"Hello": "World"}
 
 
-# ----------- BROWSER FLOW AUTH -----------
-issuer = os.environ.get("KEYCLOAK_URL")+"realms/"+os.environ.get("KEYCLOAK_REALM")
-clientId = os.environ.get("KEYCLOAK_CLIENT_ID")
-clientSecret = os.environ.get("KEYCLOAK_CLIENT_SECRET")
-oidcDiscoveryUrl = f'{issuer}/.well-known/openid-configuration'
-# from authlib.integrations.requests_client import OAuth2Session
-from authlib.integrations.requests_client import OAuth2Session
-scope = 'openid email profile'
-# scope = 'email'
-#client = OAuth2Session(clientId, clientSecret, scope=scope, redirect_uri=redirect_uri)
-client = OAuth2Session(clientId, clientSecret, scope=scope)
-#TODO: just for feasibility -> SHARED STATE https://www.vidavolta.io/sharing-memory/
-state = {} #HACK store the bearer_token for now!
+@app.get('/resource')
+def authorized_resource(
+    token: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl='token'))],
+) -> dict:
+    check_permissions(
+        token=token, permissions='myresource#read',
+    )
 
-
-import requests
-import secrets
-from urllib.parse import urlencode, quote
-#HACK
-#TODO put the helpers into a dedicated folder !WARNING! this needs to be mounted in python as well!!
-# import sys
-# # Get the absolute path of the parent folder
-# parent_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-# # Add the parent folder to the Python path
-# sys.path.append(parent_folder_path)
-from helper.oidc import check_bearer, get_discovery_response_dict
+    return {'foo': 'bar'}
 
 
 @app.get("/private")
 def get_private(request: Request):
-    try:
-        # Accessing the Authorization header
-        authorization_header = request.headers.get("Authorization")
-        if authorization_header is None:
-            raise HTTPException(status_code=401, detail="Authorization header is missing")
-        # Check if the Authorization header starts with 'Bearer'
-        if not authorization_header.startswith("Bearer "):
-            raise HTTPException(status_code=401, detail="Invalid authorization header")
-        # Extract the token after 'Bearer '
-        bearer_token = authorization_header.split(" ")[1]
-    except HTTPException as e:
-        # Catch HTTPException and perform a redirect
-        if e.status_code == 401:
-            resp = requests.get(url=oidcDiscoveryUrl)
-            auth_url = resp.json()["authorization_endpoint"]
-            #HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
-            auth_url = auth_url.replace(os.environ.get("KEYCLOAK_URL"), os.environ.get("KEYCLOAK_BROWSER_URL"))
-            response_type = 'code'
-            client_id = 'myclient'
-            redirect_uri = request.url.scheme+"://"+request.url.netloc+"/callback"
-            # Encoding the redirect_uri
-            # redirect_uri= quote(redirect_uri, safe='')
-            state = 'xyz123'
-            # Generate a random string for nonce
-            nonce_value = secrets.token_urlsafe(16)  # Adjust the length as needed (16 bytes here)
-            login_url= f"{auth_url}?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}&state={state}&nonce={nonce_value}"
-            # INFO: redirect_uri used from the client settings ->  http://localhost:8090/admin/master/console/#/myrealm/clients/daabac99-9ee9-412c-bcac-1c635d6657f5/settings
-            uri, state = client.create_authorization_url(auth_url, redirect_uri=redirect_uri)
-            return RedirectResponse(uri)
-        else:
-                # Handle other HTTPException cases here
-                raise e  # Re-raise the exception if not handled
-    return {"User-Agent": user_agent, "Content-Type": content_type}
+    return check_bearer(request)
+
 
 @app.get("/callback")
 def get_callback(request: Request):
     print("something")
-    resp= requests.get(url=oidcDiscoveryUrl)
-    token_url= resp.json()["token_endpoint"]
-    redirect_uri = request.url.scheme+"://"+request.url.netloc+"/callback"
-    token_resp = client.fetch_token(url=token_url, authorization_response=request.url._url, redirect_uri= redirect_uri, authorization_code='authorization_code')
+    client = OAuthClient()
+    resp = requests.get(url=client.oidcDiscoveryUrl)
+    token_url = resp.json()["token_endpoint"]
+    redirect_uri = request.url.scheme + "://" + request.url.netloc + "/callback"
+    token_resp = client.oidc_client.fetch_token(url=token_url, authorization_response=request.url._url,
+                                                redirect_uri=redirect_uri,
+                                                authorization_code='authorization_code')
     # https://github.com/lepture/authlib/blob/c8f154ff35459f79cc04f4c214601d25d716ddd4/docs/specs/rfc7636.rst#L83
     state['bearer_token'] = token_resp['access_token']
     return RedirectResponse('/me')
 
-# https://fastapi.tiangolo.com/advanced/custom-response/#html-response
-from fastapi.responses import HTMLResponse
+
 @app.get("/me", response_class=HTMLResponse)
 def get_me(request: Request):
-    resp= requests.get(url=oidcDiscoveryUrl)
+    client = OAuthClient()
+    resp = requests.get(url=client.oidcDiscoveryUrl)
     userinfo_endpoint = resp.json()["userinfo_endpoint"]
-    #HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
-    userinfo_endpoint = userinfo_endpoint.replace(os.environ.get("KEYCLOAK_URL"), os.environ.get("KEYCLOAK_BROWSER_URL"))
+    # HACK for browser redirect outside the container environment we need to replace `host.docker.internal` with `localhost`
+    userinfo_endpoint = userinfo_endpoint.replace(os.environ.get("KEYCLOAK_URL"),
+                                                  os.environ.get("KEYCLOAK_BROWSER_URL"))
     javascript_code = '''
         let endpointUrl = '{{url}}';
         const bearerToken = '{{token}}';
@@ -129,7 +114,7 @@ def get_me(request: Request):
 
     '''
     # Replace placeholders with actual values using str.replace()
-    javascript_code = javascript_code.replace('{{url}}',  userinfo_endpoint).replace('{{token}}', state['bearer_token'])
+    javascript_code = javascript_code.replace('{{url}}', userinfo_endpoint).replace('{{token}}', state['bearer_token'])
     return """
         <html>
             <head>
@@ -145,18 +130,16 @@ def get_me(request: Request):
         </html>
     """.format(javascript_code=javascript_code)
 
-from helper.oidc import verify_jwt
-from authlib.jose import jwt
-from authlib.jose.errors import *
+
 @app.get("/verify")
 def get_callback(request: Request):
     print("something")
     try:
-        token= check_bearer(request)
-        #verify_jwt(state['bearer_token'])
-        verify_jwt(token)
+        # token = check_bearer(request)
+        verify_jwt(state['bearer_token'])
+        # verify_jwt(token)
         return {"Hello": "/verify"}
-    #TODO: get a more detailed exception
+    # TODO: get a more detailed exception
     except ExpiredTokenError:
         # Token has expired
         raise HTTPException(status_code=401, detail="Invalid JWT: Token has expired")
@@ -170,37 +153,55 @@ def get_callback(request: Request):
         # Invalid token
         raise HTTPException(status_code=401, detail="Invalid JWT: Token is not valid")
 
-# ----------- UMA -----------
-from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection, KeycloakUMA
-from fastapi.responses import JSONResponse
+
 @app.get("/uma")
 def get_uma():
-    # do we have all env vars ready?
-    # https://github.com/marcospereirampj/python-keycloak/blob/bc810d17cbd66bc6315409508aec386c6b8180b1/tests/conftest.py#L509
-    connection = KeycloakOpenIDConnection(
-        server_url=os.environ.get("KEYCLOAK_URL"),
-        realm_name=os.environ.get("KEYCLOAK_REALM"),
-        client_id=os.environ.get("KEYCLOAK_CLIENT_ID"),
-        client_secret_key=os.environ.get("KEYCLOAK_CLIENT_SECRET"),
-        timeout=20,
-    )
-
-    # https://github.com/marcospereirampj/python-keycloak/blob/bc810d17cbd66bc6315409508aec386c6b8180b1/tests/conftest.py#L530C11-L530C45
-    uma = KeycloakUMA(connection=connection)
+    client = OAuthClient()
     # https://github.com/marcospereirampj/python-keycloak/blob/master/tests/test_keycloak_uma.py
-    resource_sets = uma.resource_set_list()
+    resource_sets = client.uma.resource_set_list()
     resource_set_list = list(resource_sets)
     return JSONResponse(content={"data": resource_set_list})
+
 
 # TODO: not working, yet!
 @app.post("/uma")
 def set_uma():
+    client = OAuthClient()
     # https://github.com/marcospereirampj/python-keycloak/blob/bc810d17cbd66bc6315409508aec386c6b8180b1/tests/test_keycloak_uma.py#L92
     resource_to_create = {
         "name": "mytest",
         "scopes": ["test:read", "test:write"],
         "type": "urn:test",
     }
-    created_resource = uma.resource_set_create(resource_to_create)
-    get_created_resource = uma.resource_set_read(created_resource["_id"])
+    created_resource = client.uma.resource_set_create(resource_to_create)
+    get_created_resource = client.uma.resource_set_read(created_resource["_id"])
 
+
+@app.post('/auth')
+def authorize(auth_data: AuthData):
+    keycloak_client = KeycloakClient()
+    token = keycloak_client.oidc_client.token(**auth_data.model_dump())
+    return token
+
+
+# KeycloakClient
+@app.get('/uma_resources_list')
+def get_uma_resource_set_list():
+    keycloak_client = KeycloakClient()
+    return keycloak_client.uma.resource_set_list()
+
+
+def check_permissions(token: str, permissions: str) -> None:
+    keycloak_client = KeycloakClient()
+    try:
+        keycloak_client.oidc_client.uma_permissions(
+            token,
+            permissions=permissions,
+        )
+    except KeycloakPostError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+
+if __name__ == '__main__':
+    # For debug purposes
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
